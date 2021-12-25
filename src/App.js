869dfb4429dab0f40c1a3a81c7815e9de2397c39
Lydia Goulding <lydia.p.goulding+git@gmail.com>
2021-12-24 20:15:40 -0600

    Finish sidebar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import Sidebar from './Sidebar';
 import './App.css';
-import CollapsableList from "./CollapsableList";
 
 function App() {
   return (
     <div className="App">
       <Sidebar></Sidebar>
-      <CollapsableList header="CollapsableList">
-          <ul>
-              <li>Foobar</li>
-              <li>Foobar</li>
-              <li>Foobar</li>
-          </ul>
-          <CollapsableList
-              header="InnerCollapsable"
-              headerClasses="embiggen"
-          >
-              <ul>
-                  <li>Foobar</li>
-                  <li>Foobar</li>
-                  <li>Foobar</li>
-              </ul>
-          </CollapsableList>
-      </CollapsableList>
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,10 +5,14 @@ import { useState } from 'react';
 
 export default function Sidebar() {
     const [reactNmiCollectJSCollapsed, setReactNmiCollectJSCollapsed] = useState(false);
+    const [componentsCollapsed, setComponentsCollapsed] = useState(true);
+    const [hooksCollapsed, setHooksCollapsed] = useState(true);
+    const [contextCollapsed, setContextCollapsed] = useState(true);
+    const [utilitiesCollapsed, setUtilitiesCollapsed] = useState(true);
 
     return (
         <div className="sidebar">
-            <ul className={`sidebar__level1  collapsable ${!reactNmiCollectJSCollapsed && 'collapsable--open'}`}>
+            <ul className={`sidebar__level1 collapsable ${!reactNmiCollectJSCollapsed && 'collapsable--open'}`}>
                 <li className="sidebar__h1" onClick={() => setReactNmiCollectJSCollapsed(!reactNmiCollectJSCollapsed)}>
                     <div>
                         <FontAwesomeIcon icon={faReact} size="lg"/>
@@ -19,27 +23,36 @@ export default function Sidebar() {
                 </li>
                 <ul className="collapsable__content sidebar__level2">
                     <li>Example</li>
-                    <li>Components</li>
-                    <ul className="sidebar__level3">
-                        <li>CardCVVInput</li>
-                        <li>CardExpirationInput</li>
-                        <li>CardNumberInput</li>
-                        <li>CheckAccountInput</li>
-                        <li>CheckRoutingNumberInput</li>
-                        <li>CheckNameInput</li>
-                        <li>ValidationContainer</li>
+                    <li onClick={() => setComponentsCollapsed(!componentsCollapsed)}>Components</li>
+                    <ul className={`sidebar__level3 collapsable ${!componentsCollapsed && 'collapsable--open'}`}>
+                        <div className="collapsable__content">
+                            <li>CardCVVInput</li>
+                            <li>CardExpirationInput</li>
+                            <li>CardNumberInput</li>
+                            <li>CheckAccountInput</li>
+                            <li>CheckRoutingNumberInput</li>
+                            <li>CheckNameInput</li>
+                            <li>ValidationContainer</li>
+                        </div>
+
                     </ul>
-                    <li>Hooks</li>
-                    <ul className="sidebar__level3">
-                        <li>useCollect</li>
+                    <li onClick={() => setHooksCollapsed(!hooksCollapsed)}>Hooks</li>
+                    <ul className={`sidebar__level3 collapsable ${!hooksCollapsed && 'collapsable--open'}`}>
+                        <div className="collapsable__content">
+                            <li>useCollect</li>
+                        </div>
                     </ul>
-                    <li>Context</li>
-                    <ul className="sidebar__level3">
-                        <li>CollectJSProvider</li>
+                    <li onClick={() => setContextCollapsed(!contextCollapsed)}>Context</li>
+                    <ul className={`sidebar__level3 collapsable ${!contextCollapsed && 'collapsable--open'}`}>
+                        <div className="collapsable__content">
+                            <li>CollectJSProvider</li>
+                        </div>
                     </ul>
-                    <li>Utilities</li>
-                    <ul className="sidebar__level3">
-                        <li>injectCollectJS</li>
+                    <li onClick={() => setUtilitiesCollapsed(!utilitiesCollapsed)}>Utilities</li>
+                    <ul  className={`sidebar__level3 collapsable ${!utilitiesCollapsed && 'collapsable--open'}`}>
+                        <div className="collapsable__content">
+                            <li>injectCollectJS</li>
+                        </div>
                     </ul>
                 </ul>
             </ul>
