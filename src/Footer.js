f277f65e7f7df6b5a1e869ca073f3bb44fba6691
Lydia Goulding <lydia.p.goulding+git@gmail.com>
2021-12-29 17:05:23 -0600

    Add content to a footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,37 @@
 import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faNpm } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
     return (
-        <div className="footer"></div>
+        <div className="footer">
+            <div className="footer__grid">
+                <div>
+                    <div className="footer__columnHeader">lydiagoulding.github.io</div>
+                    <div className="footer__columnItem">
+                        <a href="https://github.com/lydiagoulding/lydiagoulding.github.io">
+                            <FontAwesomeIcon fixedWidth icon={faGithub} />
+                            <span className="footer__columnItemText">Github</span>
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <div className="footer__columnHeader">react-nmi-collectjs</div>
+                    <div className="footer__columnItem">
+                        <a href="https://github.com/lydiagoulding/react-nmi-collectjs">
+                            <FontAwesomeIcon fixedWidth icon={faGithub} />
+                            <span className="footer__columnItemText">Github</span>
+                        </a>
+                    </div>
+                    <div className="footer__columnItem">
+                        <a href="https://www.npmjs.com/package/@lydiagoulding/react-nmi-collectjs">
+                            <FontAwesomeIcon fixedWidth icon={faNpm} />
+                            <span className="footer__columnItemText">npm</span>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
